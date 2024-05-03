9e62ebd2a0dccf3f5736f21d8309f27f9a269f5c
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-03 15:18:40 +0200

    M-1  2/2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // MILESTONE 1 - GENERARE 5 NUMERI CASUALI
 let n_array = [];
+let ul = document.querySelector(`ul`);
 
 for (let i = 0; i < 5; i++) {
-	let n_random = Math.floor(Math.random() * 5 + 1);
+	let li = document.createElement(`li`);
+	let n_random = Math.floor(Math.random() * 100 + 1);
 	n_array.push(n_random);
-	console.log(n_random, n_array);
+	li.append(n_random);
+	ul.append(li);
 }
